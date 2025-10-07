2cde24c0c22f0efafbecf366443c38c2e5d0b042
bakerko <ua.nikutin@gmail.com>
2025-10-07 21:06:00 +0200

    neu wordschatz

---
+++ dictionary3_ru.js
@@ -0,0 +1,147 @@
+
+const vocabularyDictionary = [
+    {
+        "german": "ablenken",
+        "translation": "отвлекать",
+        "synonyms": ["hindern"],
+        "synonymsTranslation": ["мешать", "препятствовать"]
+    },
+    {
+        "german": "anstreben",
+        "translation": "стремиться",
+        "synonyms": ["kämpfen für"],
+        "synonymsTranslation": ["бороться за"]
+    },
+    {
+        "german": "beheben",
+        "translation": "устранять",
+        "synonyms": ["lösen"],
+        "synonymsTranslation": ["решать"]
+    },
+    {
+        "german": "bekanntgeben",
+        "translation": "объявлять",
+        "synonyms": ["verkünden"],
+        "synonymsTranslation": ["провозглашать"]
+    },
+    {
+        "german": "benötigen",
+        "translation": "нуждаться",
+        "synonyms": ["brauchen"],
+        "synonymsTranslation": ["требоваться"]
+    },
+    {
+        "german": "einfrieren",
+        "translation": "замораживать",
+        "synonyms": ["auftauen"],
+        "synonymsTranslation": ["размораживать"]
+    },
+    {
+        "german": "einrichten",
+        "translation": "обставлять, устраивать",
+        "synonyms": ["möblieren"],
+        "synonymsTranslation": ["меблировать"]
+    },
+    {
+        "german": "einwilligen",
+        "translation": "соглашаться",
+        "synonyms": ["zustimmen"],
+        "synonymsTranslation": ["давать согласие"]
+    },
+    {
+        "german": "engagieren",
+        "translation": "нанимать, увлекать",
+        "synonyms": ["einsetzen"],
+        "synonymsTranslation": ["задействовать", "использовать"]
+    },
+    {
+        "german": "erreichen",
+        "translation": "достигать",
+        "synonyms": ["ziel schaffen"],
+        "synonymsTranslation": ["достигать цели"]
+    },
+    {
+        "german": "hinwegkommen",
+        "translation": "справляться, переживать",
+        "synonyms": ["verschon sein", "überstehen"],
+        "synonymsTranslation": ["избежать", "выдерживать"]
+    },
+    {
+        "german": "meiden",
+        "translation": "избегать",
+        "synonyms": ["ausweichen"],
+        "synonymsTranslation": ["уворачиваться"]
+    },
+    {
+        "german": "nachholen",
+        "translation": "наверстывать",
+        "synonyms": ["nachmachen"],
+        "synonymsTranslation": ["подражать"]
+    },
+    {
+        "german": "nachwachsen",
+        "translation": "отрастать заново",
+        "synonyms": ["bildet sich nach"],
+        "synonymsTranslation": ["восстанавливаться"]
+    },
+    {
+        "german": "pendeln",
+        "translation": "колебаться, курсировать",
+        "synonyms": ["hin und her fahren"],
+        "synonymsTranslation": ["ездить туда-сюда"]
+    },
+    {
+        "german": "reduzieren",
+        "translation": "сокращать",
+        "synonyms": ["weniger machen"],
+        "synonymsTranslation": ["уменьшать"]
+    },
+    {
+        "german": "scheiden",
+        "translation": "отделять, уходить",
+        "synonyms": ["abgehen", "abgrenzen", "trennen"],
+        "synonymsTranslation": ["уходить", "разграничивать", "разделять"]
+    },
+    {
+        "german": "unterstützen",
+        "translation": "поддерживать",
+        "synonyms": ["helfen"],
+        "synonymsTranslation": ["помогать"]
+    },
+    {
+        "german": "verarbeiten",
+        "translation": "перерабатывать, обрабатывать",
+        "synonyms": ["etw. herstellen aus einem Material", "daran denken"],
+        "synonymsTranslation": ["изготавливать что-л. из материала", "помнить об этом"]
+    },
+    {
+        "german": "verringern",
+        "translation": "уменьшать",
+        "synonyms": ["weniger machen", "vermehren"],
+        "synonymsTranslation": ["сокращать", "умножать"]
+    },
+    {
+        "german": "versperren",
+        "translation": "преграждать",
+        "synonyms": ["verschließen"],
+        "synonymsTranslation": ["запирать"]
+    },
+    {
+        "german": "verzichten",
+        "translation": "отказываться",
+        "synonyms": ["meiden"],
+        "synonymsTranslation": ["избегать"]
+    },
+    {
+        "german": "wohlfühlen",
+        "translation": "чувствовать себя хорошо",
+        "synonyms": ["gut fühlen"],
+        "synonymsTranslation": ["хорошо себя чувствовать"]
+    },
+    {
+        "german": "zusichern",
+        "translation": "гарантировать",
+        "synonyms": ["versprechen"],
+        "synonymsTranslation": ["обещать"]
+    }
+];
---
+++ dictionary4_ru.js
@@ -0,0 +1,75 @@
+
+const vocabularyDictionary = [
+    {
+        "german": "alljährlich",
+        "translation": "ежегодный",
+        "synonyms": ["jedes Jahr"],
+        "synonymsTranslation": ["каждый год"]
+    },
+    {
+        "german": "fällig",
+        "translation": "подлежащий оплате, срок которого истёк",
+        "synonyms": ["gültig bis"],
+        "synonymsTranslation": ["действительный до"]
+    },
+    {
+        "german": "fleischlos",
+        "translation": "безмясной",
+        "synonyms": ["ohne Fleisch"],
+        "synonymsTranslation": ["без мяса"]
+    },
+    {
+        "german": "hauptsächlich",
+        "translation": "главным образом",
+        "synonyms": ["grundsätzlich"],
+        "synonymsTranslation": ["в принципе", "принципиально"]
+    },
+    {
+        "german": "porös",
+        "translation": "пористый",
+        "synonyms": ["löchrig"],
+        "synonymsTranslation": ["дырчатый"]
+    },
+    {
+        "german": "talentiert",
+        "translation": "талантливый",
+        "synonyms": ["begabt"],
+        "synonymsTranslation": ["одарённый"]
+    },
+    {
+        "german": "unbenutzbar",
+        "translation": "непригодный к использованию",
+        "synonyms": ["ungebräuchlich"],
+        "synonymsTranslation": ["неупотребительный"]
+    },
+    {
+        "german": "ungeordnet",
+        "translation": "неупорядоченный",
+        "synonyms": ["chaotisch"],
+        "synonymsTranslation": ["хаотичный"]
+    },
+    {
+        "german": "vermehrt",
+        "translation": "в увеличенном количестве",
+        "synonyms": ["mehr"],
+        "synonymsTranslation": ["больше"]
+    },
+    {
+        "german": "zuständig",
+        "translation": "компетентный, ответственный",
+        "synonyms": ["verantwortlich für"],
+        "synonymsTranslation": ["ответственный за"]
+    },
+    {
+        "german": "ehebaldigst",
+        "translation": "в ближайшее время",
+        "synonyms": ["so schnell wie möglich"],
+        "synonymsTranslation": ["как можно скорее"]
+    },
+    {
+        "german": "jeglicher",
+        "translation": "любой, всякий",
+        "synonyms": ["jede einzelne"],
+        "synonymsTranslation": ["каждый отдельный"]
+    }
+];
